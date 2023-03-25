7b4866be9bb0793333706339aa6eb2388775bac1
Oleksii Lavreninv <oleksii.lavrenin@gmail.com>
2023-03-24 21:15:26 -0400

    Updated header and footer. Added routers

--- 3-React/nucampsite/src/components/Footer.js
+++ 3-React/nucampsite/src/components/Footer.js
@@ -1,4 +1,5 @@
-import { Container, Col, Row } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -6,9 +7,22 @@ const Footer = () => {
       <Container>
         <Row>
           <Col xs={{ size: 4, offset: 1 }} sm="2">
-            <h5>this will be site navigation!</h5>
+            <h5>Links</h5>
+            <ul className="list-unstyled">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/directory">Directory</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
           </Col>
-
           <Col xs="6" sm="3" className="text-center">
             <h5>Social</h5>
             <a
@@ -29,11 +43,13 @@ const Footer = () => {
             >
               <i className="fa fa-twitter" />
             </a>{" "}
-            <a className="btn btn-social-icon btn-" href="http://youtube.com/">
+            <a
+              className="btn btn-social-icon btn-google"
+              href="http://youtube.com/"
+            >
               <i className="fa fa-youtube" />
             </a>
           </Col>
-
           <Col sm="4" className="text-center">
             <a role="button" className="btn btn-link" href="tel:+12065551234">
               <i className="fa fa-phone" /> 1-206-555-1234
