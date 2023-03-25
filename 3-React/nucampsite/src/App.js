9615de9af738dc049eba01cca6be24907ce73d4d
Oleksii Lavreninv <oleksii.lavrenin@gmail.com>
2023-03-25 19:03:43 -0400

    Added CampsiteDetail page.

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import ContactPage from "./pages/ContactPage";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
@@ -16,6 +17,10 @@ function App() {
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
         <Route path='about' element={<AboutPage />} />
+        <Route
+            path='directory/:campsiteId'
+            element={<CampsiteDetailPage />}
+        />
       </Routes>
       <Footer />
     </div>
