26fa5d1f6329d20808e05e2ed2c3f4c929673d70
Oleksii Lavreninv <oleksii.lavrenin@gmail.com>
2023-04-08 12:53:52 -0400

    Created Login form

--- 3-React/nucampsite/src/features/comments/commentsSlice.js
+++ 3-React/nucampsite/src/features/comments/commentsSlice.js
@@ -9,14 +9,8 @@ const commentsSlice = createSlice({
   name: 'comments',
   initialState,
   reducers: {
-      addComment: (state, action) => {
-          console.log('addComment action.payload:', action.payload);
-          console.log('addComment state.commentsArray:', state.commentsArray);
-          const newComment = {
-              id: state.commentsArray.length + 1,
-              ...action.payload
-          };
-          state.commentsArray.push(newComment);
+    setCurrentUser: (state, action) => {
+          ({...state, currentUser: action.payload});
       }
   }
 });
