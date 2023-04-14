62edbcd1a47a207e7ba60ad8435092ce0c2f799f
Oleksii Lavreninv <oleksii.lavrenin@gmail.com>
2023-04-13 21:44:50 -0400

    Added async campsites.

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import { Routes, Route } from "react-router-dom";
 import ContactPage from "./pages/ContactPage";
 import AboutPage from "./pages/AboutPage";
@@ -6,9 +9,18 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import "./App.css";
 
+
+
 function App() {
+  const dispatch = useDispatch();
+
+useEffect(() => {
+  dispatch(fetchCampsites());
+}, [dispatch]);
+
   return (
     <div className="App">
       <Header />
