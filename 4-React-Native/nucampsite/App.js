c85c12e53bafa338314377580a1556a023f9375f
Oleksii Lavreninv <oleksii.lavrenin@gmail.com>
2023-05-04 00:03:04 -0400

    Completed Navigation 1

--- 4-React-Native/nucampsite/App.js
+++ 4-React-Native/nucampsite/App.js
@@ -1,7 +1,10 @@
 import Main from "./screens/MainComponent";
-import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-  return <Main />;
+  return (
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
+  );
 }
