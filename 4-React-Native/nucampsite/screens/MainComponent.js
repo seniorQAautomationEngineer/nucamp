967a1ba168b1b2a990040b14f27b98257a29a2b0
Oleksii Lavreninv <oleksii.lavrenin@gmail.com>
2023-05-04 21:59:42 -0400

    Completed React Native Assignment 1

--- 4-React-Native/nucampsite/screens/MainComponent.js
+++ 4-React-Native/nucampsite/screens/MainComponent.js
@@ -5,6 +5,8 @@ import Constants from "expo-constants";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./HomeScreen";
+import ContactScreen from "./ContactScreen";
+import AboutScreen from "./AboutScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -46,6 +48,32 @@ const DirectoryNavigator = () => {
   );
 };
 
+const AboutNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="About"
+        component={AboutScreen}
+        options={{ title: "About" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const ContactNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Contact"
+        component={ContactScreen}
+        options={{ title: "Contact Us" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const Main = () => {
   return (
     <View
@@ -68,6 +96,18 @@ const Main = () => {
           name="Directory"
           component={DirectoryNavigator}
           options={{ title: "Directory" }}
+        />
+
+        <Drawer.Screen
+          name="About"
+          component={AboutNavigator}
+          options={{ title: "About" }}
+        />
+
+        <Drawer.Screen
+          name="Contact"
+          component={ContactNavigator}
+          options={{ title: "Contact Us" }}
         />
       </Drawer.Navigator>
     </View>
