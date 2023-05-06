f1ea3dc0bf96c7760e2e37dec62f33b673b97023
Oleksii Lavreninv <oleksii.lavrenin@gmail.com>
2023-05-06 08:05:06 -0400

    Implemented forms

--- 4-React-Native/nucampsite/screens/MainComponent.js
+++ 4-React-Native/nucampsite/screens/MainComponent.js
@@ -11,6 +11,7 @@ import {
 import HomeScreen from "./HomeScreen";
 import ContactScreen from "./ContactScreen";
 import AboutScreen from "./AboutScreen";
+import ReservationScreen from "./ReservationScreen";
 import { Icon } from "react-native-elements";
 import logo from "../assets/images/logo.png";
 import { useDispatch } from "react-redux";
@@ -139,6 +140,29 @@ const ContactNavigator = () => {
   );
 };
 
+const ReservationNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Reservationn"
+        component={ReservationScreen}
+        options={({ navigation }) => ({
+          title: "Reservation Search",
+          headerLeft: () => (
+            <Icon
+              name="tree"
+              type="font-awesome"
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          ),
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const Main = () => {
   const dispatch = useDispatch();
 
@@ -185,6 +209,22 @@ const Main = () => {
             drawerIcon: ({ color }) => (
               <Icon
                 name="list"
+                type="font-awesome"
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="ReserveCampsite"
+          component={ReservationNavigator}
+          options={{
+            title: "Reserve Campsite",
+            drawerIcon: ({ color }) => (
+              <Icon
+                name="tree"
                 type="font-awesome"
                 size={24}
                 iconStyle={{ width: 24 }}
