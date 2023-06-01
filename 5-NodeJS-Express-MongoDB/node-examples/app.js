2005ffd94af6603f8f182c38a94b98d8ba6a0a53
seniorQAautomationEngineer <oleksii.lavrenin@gmail.com>
2023-05-31 22:34:01 -0400

    Callbacks and Error Handling In Node

--- 5-NodeJS-Express-MongoDB/node-examples/app.js
+++ 5-NodeJS-Express-MongoDB/node-examples/app.js
@@ -3,14 +3,18 @@ const rect = require("./rectangle");
 function solveRect(l, w) {
   console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
-  if (l <= 0 || w <= 0) {
-    console.log(
-      `Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`
-    );
-  } else {
-    console.log(`Area of rectangle: ${rect.area(l, w)}`);
-    console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
-  }
+  rect(l, w, (err, rectangle) => {
+    if (err) {
+      console.log("ERROR:", err.message);
+    } else {
+      console.log(
+        `Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`
+      );
+      console.log(
+        `Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`
+      );
+    }
+  });
 }
 
 solveRect(2, 4);
