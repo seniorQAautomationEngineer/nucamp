f71ca271e5837f83a6be9f419c6c33c63c434360
seniorQAautomationEngineer <oleksii.lavrenin@gmail.com>
2023-06-17 12:17:38 -0400

    Backend. Assignment 3.

--- 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/partnerRouter.js
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/routes/partnerRouter.js
@@ -10,7 +10,7 @@ partnerRouter
       .then((partners) => res.status(200).json(partners))
       .catch((err) => next(err));
   })
-  .post(authenticate.verifyUser, (req, res, next) => {
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Partner.create(req.body)
       .then((partner) => res.status(200).json(partner))
       .catch((err) => next(err));
@@ -19,11 +19,15 @@ partnerRouter
     res.statusCode = 403;
     res.end("PUT operation not supported on /partners");
   })
-  .delete(authenticate.verifyUser, (req, res, next) => {
-    Partner.deleteMany()
-      .then((partners) => res.status(200).json(partners))
-      .catch((err) => next(err));
-  });
+  .delete(
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Partner.deleteMany()
+        .then((partners) => res.status(200).json(partners))
+        .catch((err) => next(err));
+    }
+  );
 
 partnerRouter
   .route("/:partnerId")
@@ -32,21 +36,25 @@ partnerRouter
       .then((partner) => res.status(200).json(partner))
       .catch((err) => next(err));
   })
-  .post(authenticate.verifyUser, (req, res) => {
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     res.statusCode = 403;
     res.end(
       `POST operation not supported on /partners/${req.params.partnerId}`
     );
   })
-  .put(authenticate.verifyUser, (req, res, next) => {
+  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Partner.findByIdAndUpdate(req.params.partnerId, req.body, { new: true })
       .then((partner) => res.status(200).json(partner))
       .catch((err) => next(err));
   })
-  .delete(authenticate.verifyUser, (req, res, next) => {
-    Partner.findByIdAndDelete(req.params.partnerId)
-      .then((partner) => res.status(200).json(partner))
-      .catch((err) => next(err));
-  });
+  .delete(
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Partner.findByIdAndDelete(req.params.partnerId)
+        .then((partner) => res.status(200).json(partner))
+        .catch((err) => next(err));
+    }
+  );
 
 module.exports = partnerRouter;
